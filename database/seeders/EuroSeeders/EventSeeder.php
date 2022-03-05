229bf7a9127fbbd1c5a232cea6092a62991c8a69
Oscar Roth Andersen <oscarrothandersen@gmail.com>
2022-03-05 19:35:18 +0100

    :bug: Mapped Event seeder to WCA events (#24)

--- database/seeders/EuroSeeders/EventSeeder.php
+++ database/seeders/EuroSeeders/EventSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders\EuroSeeders;
 
 use App\Models\Competition;
 use App\Models\Event;
+use App\Services\Wca\Enums\Event as EventEnum;
 use Illuminate\Database\Seeder;
 
 class EventSeeder extends Seeder
@@ -16,25 +17,29 @@ class EventSeeder extends Seeder
     public function run()
     {
         $events = collect([
-            '2x2x2',
-            '3x3x3',
-            '4x4x4',
-            '5x5x5',
-            '6x6x6',
-            '7x7x7',
-            '3x3x3 One-handed',
-            '3x3x3 Blindfolded',
-            '4x4x4 Blindfolded',
-            '5x5x5 Blindfolded',
-            '3x3x3 Fewest moves',
-            'Pyraminx',
-            'Megaminx',
-            'Square-1',
-            'Clock',
-            'Skewb',
+            '222',
+            '333',
+            '444',
+            '555',
+            '666',
+            '777',
+            '3oh',
+            '3bld',
+            '4bld',
+            '5bld',
+            'fmc',
+            'pyram',
+            'minx',
+            'sq1',
+            'clock',
+            'skewb',
         ]);
 
         $euro = Competition::first();
-        $events->each(Fn (string $event) => Event::factory()->create(['competition_id' => $euro->id, 'title' => $event]));
+        $events->each(Fn (string $event) => Event::factory()->create([
+            'competition_id' => $euro->id,
+            'wca_event_id' => $event,
+            'title' => EventEnum::from($event),
+        ]));
     }
 }
