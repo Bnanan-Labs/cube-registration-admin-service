f17a86efee9beb46759a984d1951b4fce1df8f3b
Oscar Roth Andersen <oscarrothandersen@gmail.com>
2022-03-17 21:18:23 +0100

    :sparkles: Rejecting duplicate registrations on register endpoint (#34)

--- app/GraphQL/Mutations/RegisterCompetitor.php
+++ app/GraphQL/Mutations/RegisterCompetitor.php
@@ -2,24 +2,32 @@
 
 namespace App\GraphQL\Mutations;
 
+use GraphQL\Error\Error;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
 class RegisterCompetitor
 {
     /**
-     * @param  null  $_
-     * @param  array<string, mixed>  $args
+     * @param null $_
+     * @param array<string, mixed> $args
+     * @throws Error
      */
     public function __invoke($_, array $args)
     {
-        \App\Jobs\RegisterCompetitor::dispatch($args, Auth::user(), Request::ip());
+        /* @var $user \App\Models\User */
+        $user = Auth::user();
+        if ($user->is_competitor) {
+            throw new Error('You already have a pending registration');
+        }
+
+        \App\Jobs\RegisterCompetitor::dispatch($args, $user, Request::ip());
 
         return [
             'id' => 1234,
             'first_name' => $args['first_name'],
             'last_name' => $args['last_name'],
-            'wca_id' => Auth::user()->wca_id,
+            'wca_id' => $user->wca_id,
             'email' => $args['email'],
         ];
     }
--- tests/Feature/GraphQL/RegistrationTest.php
+++ tests/Feature/GraphQL/RegistrationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\GraphQL;
 
 use App\Jobs\RegisterCompetitor;
+use App\Models\Competitor;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Queue;
@@ -54,5 +55,48 @@ class RegistrationTest extends GraphQLTestCase
 
         Queue::assertPushed(Fn (RegisterCompetitor $job) => $job->registration['email'] === $registration['email']);
         Queue::assertPushed(Fn (RegisterCompetitor $job) => $job->user['wca_id'] === $user->wca_id);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCanRejectRegistration()
+    {
+        /** @var User $user */
+        $user = User::factory()->create(['wca_id' => '2010TEST01']);
+        $this->authenticate($user);
+        Competitor::factory()->create(['wca_id' => '2010TEST01']);
+        Queue::fake();
+        Queue::assertNothingPushed();
+        $registration = [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->email(),
+            'guests' => [$this->faker->name(), $this->faker->name()],
+            'is_interested_in_nations_cup' => $this->faker->boolean(),
+            'events' => [$this->faker->numberBetween(1,10), $this->faker->numberBetween(1,10)],
+            'days' => [$this->faker->numberBetween(1,10), $this->faker->numberBetween(1,10)],
+        ];
+
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation ($input: RegisterCompetitorInput!)
+            {
+                registerCompetitor(input: $input) {
+                    id
+                }
+            }
+        ', [
+            'input' => $registration
+        ])->assertJSON([
+            'errors' => [
+                [
+                    'message' => 'You already have a pending registration',
+                ],
+            ],
+        ]);
+
+        Queue::assertNothingPushed();
     }
 }
